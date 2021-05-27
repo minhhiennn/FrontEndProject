bf98754fd4f2cb0a943088d15bb165bb7c0fec68
damanh1905 <18130005@st.hcmuaf.edu.vn>
2021-05-27 15:28:07 +0700

    tao component

--- AngularProject/src/app/app.module.ts
+++ AngularProject/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './Component/cart/cart.component';
+import { BlogComponent } from './Component/blog/blog.component';
+import { CheckoutComponent } from './Component/checkout/checkout.component';
+import { ContactUsComponent } from './Component/contact-us/contact-us.component';
+import { IndexComponent } from './Component/index/index.component';
+import { LoginComponent } from './Component/login/login.component';
+import { ShopComponent } from './Component/shop/shop.component';
+import { ProductdetailComponent } from './Component/productdetail/productdetail.component';
+import { BlogSingComponent } from './Component/blog-sing/blog-sing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    BlogComponent,
+    CheckoutComponent,
+    ContactUsComponent,
+    IndexComponent,
+    LoginComponent,
+    ShopComponent,
+    ProductdetailComponent,
+    BlogSingComponent
   ],
   imports: [
     BrowserModule,
