4b94d2300fb86252a8dc7d15676bd33a4013a4d9
shegga9x <74436981+shegga9x@users.noreply.github.com>
2021-07-09 22:44:35 +0700

    update ship

--- AngularProject/src/app/service/city.service.ts
+++ AngularProject/src/app/service/city.service.ts
@@ -21,16 +21,14 @@ export class CityService {
         let x1: any = Object.values(data)[9];
         let cityArr: City[] = [];
         let x2: any = (Object.values(data1)[5])
-        console.log(x2)
-        console.log(x1)
         for (let index = 0; index < x1.length; index++) {
           for (let index1 = 0; index1 < x2.length; index1++) {
             let name: string = x2[index1]['name'];
             let value: number = x1[index]['value'];
-            let hckey: number = x1[index]['hc-key'];
-            let heckey: number = x2[index1]['hec-key'];
+            let hckey: string = x1[index]['hc-key'];
+            let heckey: string = x2[index1]['hec-key'];
             if (hckey == heckey) {
-              cityArr.push(new City(name, value, hckey, this.getCoordinate(hckey)))
+              cityArr.push(new City(name, value, hckey, this.getCordianate(hckey)))
             }
           }
         }
@@ -38,208 +36,195 @@ export class CityService {
       })
     });
   }
-<<<<<<< HEAD
-  getCoordinate(hcKey: number): string {
-=======
-  getCordianate(hcKey: String|number): string {
->>>>>>> 62fac7f4a3c0f27605275524873ddd09a0df9754
+  getCordianate(hcKey: string): string {
     let cordinate: string = "";
     switch (hcKey) {
-      case 30:
-                                                               cordinate = "20.939225337630926, 106.31275473807591";
-                                                           break;
-                                                       case 1:
-                                               cordinate = "21.03443273235992, 105.82254345647135";
-                                            break;
-                                         case 48:
-                                 cordinate = "16.057723866798206, 108.17838121453185";
-                            break;
-                       case 77:
-                  cordinate = "10.28346637496381, 106.03150239673647";
-              break;
-           case 49:
-         cordinate = "15.677823173416828, 107.61436382449152";
-           break;
-               case 74:
-                 cordinate = "11.284423314418255, 106.3652993094015";
-                     break;
-                        case 26:
-                             cordinate = "21.457557333791172, 105.39551102583525";
-                               break;
-                                 case 20:
-                                   cordinate = "21.854594515782832, 106.71713318030874";
-                                     break;
-                                       case 42:
-                                         cordinate = "18.37998194796159, 105.85942924074982";
-                                      break;
-                                   case 22:
-                              cordinate = "21.34784813334017, 106.51270726269973";
-                           break;
-                         case 72:
-                   cordinate = "20.425913954821052, 106.50965885608406";
-                       break;
-                          case 33:
-                             cordinate = "20.677434589668074, 106.03413411525838";
-                                  break;
-                               case 91:
-                                  cordinate = "9.994011372733805, 104.16893714363025";
-                            break;
-                               case 34:
-                           cordinate = "20.46033624681186, 106.30705766832301";
-                     break;
-               case 24:
-        cordinate = "21.302067651284702, 106.17949140796979";
+      case "79":
+        cordinate = "10.823512334224576,106.61446278562542";
         break;
-<<<<<<< HEAD
-      case 79:
-        cordinate = "10.826760025273845,106.6823606966149";
+      case "36":
+        cordinate = "20.43834787202179,106.16174700901708";
         break;
-      case 36:
-        cordinate = "20.43448380422534, 106.08793192698147";
+      case "94":
+        cordinate = "9.603951563293295,105.97352026584757";
         break;
-      case 89:
-        cordinate = "10.674590124286668, 105.01607942299779";
+      case "45":
+        cordinate = "16.740318537597823,107.18725145529353";
         break;
-      case 94:
-        cordinate = "9.626460029135632, 105.94159052267136";
+      case "24":
+        cordinate = "21.281438448611333,106.19871816830488";
         break;
-      case 86:
-        cordinate = "10.271819699230967, 105.90716365847219";
+      case "27":
+        cordinate = "21.177219407714173,106.06355976798004";
         break;
-      case 45:
-        cordinate = "16.725182613065336, 107.09077493453262";
+      case "30":
+        cordinate = "20.938266957772193,106.3168752998917";
         break;
-      case 31:
-        cordinate = "20.85395218342969, 106.69393949260603";
+      case "01":
+        cordinate = "21.028187499645618,105.83713536136611";
         break;
-      case 84:
-        cordinate = "9.97173222073517, 106.34073321362116";
+      case "48":
+        cordinate = "16.055087272295676,108.19915294989171";
         break;
-      case 19:
-        cordinate = "21.585663568200832, 105.7900973667736";
-=======
-                       case 79:
-                 cordinate = "10.826760025273845,106.6823606966149";
+      case "77":
+        cordinate = "10.43118714317501,107.18587531544753";
         break;
-                  case 36:
-                 cordinate = "20.43448380422534, 106.08793192698147";
+      case "74":
+        cordinate = "11.129636724208261,106.61665872678057";
         break;
-                     case 89:
-                         cordinate = "10.674590124286668, 105.01607942299779";
-                           break;
-                             case 94:
-                          cordinate = "9.626460029135632, 105.94159052267136";
-    break;
-                             case 86:
-                                cordinate = "10.271819699230967, 105.90716365847219";
-                                     break;
-                                       case 45:
-                                              cordinate = "16.725182613065336, 107.09077493453262";
-                                                  break;
-                                                case 31:
-                                     cordinate = "20.85395218342969, 106.69393949260603";
-                                         break;
-                                   case 84:
-                                 cordinate = "9.97173222073517, 106.34073321362116";
-                           break;
-                         case 19:
-                   cordinate = "21.585663568200832, 105.7900973667736";
->>>>>>> 62fac7f4a3c0f27605275524873ddd09a0df9754
+      case "26":
+        cordinate = "21.307506785915034,105.6166806160755";
         break;
-      case 96:
-        cordinate = "9.200776642645451, 105.1682859785899";
+      case "20":
+        cordinate = "21.85592254308607,106.7754793111749";
         break;
-      case 25:
-        cordinate = "21.45126547048769, 105.24112478720403";
+      case "42":
+        cordinate = "18.35619948187832,105.89050064983759";
         break;
-      case 51:
-        cordinate = "15.168344519876664, 108.81796793149348";
+      case "22":
+        cordinate = "20.97238341588559,107.03433634532294";
         break;
-      case 46:
-        cordinate = "16.41211210909523, 107.35117449778903";
+      case "72":
+        cordinate = "11.332875346182131,106.11214414065809";
         break;
-      case 15:
-        cordinate = "16.070370406916023, 108.2213519715551";
+      case "33":
+        cordinate = "20.634725712230154,106.04347826849967";
         break;
-      case 10:
-        cordinate = "22.465081276319474, 103.98806683937451";
+      case "91":
+        cordinate = "10.027000728010018,105.10575286351363";
         break;
-      case 2:
-        cordinate = "22.79424724564605, 104.98187279046168";
+      case "34":
+        cordinate = "20.457123076745482,106.34224894087464";
         break;
-      case 8:
-        cordinate = "21.82260168132014, 105.16175223082817";
+      case "11":
+        cordinate = "21.48515555103137,103.0803376711742";
         break;
-      case 12:
-        cordinate = "22.399630554854806, 103.43726575924691";
+      case "35":
+        cordinate = "20.532513505051835,105.91645434459818";
         break;
-      case 14:
-        cordinate = "21.367293656643824, 103.88760460682506";
+      case "95":
+        cordinate = "9.295488749047397,105.75210003180341";
         break;
-      case 52:
-        cordinate = "14.392838068725473, 108.61966876769002";
+      case "37":
+        cordinate = "20.251561567365325,105.97287662938912";
         break;
-      case 68:
-        cordinate = "11.59527958613712, 107.52522870768796";
+      case "17":
+        cordinate = "20.83749894607004,105.34636450000002";
         break;
-      case 70:
-        cordinate = "11.973622020945202, 106.56716684448499";
+      case "40":
+        cordinate = "18.76536363944038,105.80177870014425";
         break;
-      case 93:
-        cordinate = "10.808445894915405, 106.66219567492925";
+      case "92":
+        cordinate = "10.055518877113279,105.73904991333711";
         break;
-      case 44:
-        cordinate = "17.78181165280456, 105.75557662343996";
+      case "87":
+        cordinate = "10.753241387475523,105.6759921207901";
         break;
-      case 67:
-        cordinate = "12.456596934192778, 107.4985013731356";
+      case "38":
+        cordinate = "19.82352442367533,105.79908546554098";
         break;
-      case 62:
-        cordinate = "14.378777528603374, 107.89771263125483";
+      case "89":
+        cordinate = "10.681333815618007,105.26730930444718";
         break;
-
-      case 4:
-        cordinate = "22.667769729877488, 106.2383414869104";
+      case "86":
+        cordinate = "10.237362030807514,105.95557289114532";
         break;
-
-      case 83:
-        cordinate = "10.252595778308848, 106.35889952074201";
+      case "31":
+        cordinate = "20.847276831222473,106.68843933535854";
         break;
-
-      case 82:
-        cordinate = "10.370522332165955, 106.27205399909455";
+      case "84":
+        cordinate = "9.95561953332926,106.3351833919193";
         break;
-
-      case 80:
-        cordinate = "10.814107455203564, 105.91096580739524";
+      case "19":
+        cordinate = "21.56466960182076,105.82871867050936";
         break;
-
-      case 54:
-        cordinate = "13.275510070710544, 108.95509773456196";
+      case "96":
+        cordinate = "9.152316568378048,105.19918794562679";
         break;
-
-      case 64:
-        cordinate = "13.980260321688636, 108.12070947081207";
+      case "25":
+        cordinate = "21.421389018668293,105.23563230917952";
         break;
-
-      case 66:
-        cordinate = "12.99082837036432, 108.31651306922694";
+      case "51":
+        cordinate = "15.122790636526764,108.79820973566663";
         break;
-
-      case 58:
-        cordinate = "11.815899678695951, 108.80829863886574";
+      case "46":
+        cordinate = "16.516338819291615,107.80715310745907";
         break;
-
-      case 56:
-        cordinate = "12.41740870150259, 109.25576237008224";
+      case "15":
+        cordinate = "21.702102769257216,104.92738116149555";
         break;
-
-      case 60:
-        cordinate = "10.886979038058973, 107.63844834617815";
+      case "10":
+        cordinate = "22.479372725790707,103.98497966371512";
         break;
-
-
-
+      case "02":
+        cordinate = "22.80502343453076,104.98874196803415";
+        break;
+      case "08":
+        cordinate = "21.776394577354854,105.23110616814246";
+        break;
+      case "12":
+        cordinate = "22.386619554991572,103.47674855507701";
+        break;
+      case "14":
+        cordinate = "21.349216274556,103.97263737494829";
+        break;
+      case "52":
+        cordinate = "13.917241925729401,109.31384955656371";
+        break;
+      case "68":
+        cordinate = "12.005064314910857,108.29698845501021";
+        break;
+      case "93":
+        cordinate = "9.766620985953438,105.63148906904115";
+        break;
+      case "44":
+        cordinate = "17.551167261502368,106.67393903070499";
+        break;
+      case "67":
+        cordinate = "12.042599393281298,107.7153032915683";
+        break;
+      case "83":
+        cordinate = "10.285065007016481,106.37529807627469";
+        break;
+      case "62":
+        cordinate = "14.37346664669782,108.02302829622232";
+        break;
+      case "04":
+        cordinate = "22.660486876365653,106.26306140464389";
+        break;
+      case "82":
+        cordinate = "10.46280810355318,106.24797178621658";
+        break;
+      case "80":
+        cordinate = "10.761251370707084,106.10652056149092";
+        break;
+      case "54":
+        cordinate = "13.113000977466402,109.11459037343597";
+        break;
+      case "64":
+        cordinate = "14.040579924935585,108.14669708120257";
+        break;
+      case "66":
+        cordinate = "12.757380741065708,108.08923812264851";
+        break;
+      case "58":
+        cordinate = "11.75957427136036,108.85915746770932";
+        break;
+      case "56":
+        cordinate = "12.337497738852939,108.95383006198269";
+        break;
+      case "56":
+        cordinate = "12.337497738852939,108.95383006198269";
+        break;
+      case "vn-307":
+        cordinate = "11.08431940859576,108.10006693941291";
+        break;
+      case "truongsa":
+        cordinate = "10.725305757600497,115.82595051590171";
+        break;
+      case "hs01":
+        cordinate = "10.725474420122634,115.82663716136612";
+        break;
       default:
         break;
     }
@@ -268,15 +253,17 @@ export class CityService {
     let lon2: number = parseFloat(cordinate.split(",")[1]);
     return this.calDistance(lat1, lon1, lat2, lon2);
   }
-  getShipCost(hckey: number): number {
-    if (hckey == 79) return 0
-    return Math.round(this.splitCor(this.getCoordinate(hckey))) * 3000;
+
+  getShipCost(hckey: string): number {
+    if (hckey == "79") return 0
+    return Math.round(this.splitCor(this.getCordianate(hckey))) * 3000
   }
-  getTime(hckey: number): string {
-    if (hckey == 79) return "Trong vòng 2 đến 3 ngày"
-    if (this.splitCor(this.getCoordinate(hckey)) > 100) return "Trong 1 đến 2 ngày"
-    else if (this.splitCor(this.getCoordinate(hckey)) > 500) return "Trong 3 đến 4 ngày"
-    else if (this.splitCor(this.getCoordinate(hckey))) return "Trong 4 đến 5 ngày"
+  getTime(hckey: string): string {
+    if (hckey == "79") return "Từ 2 đến 3 tiếng"
+    else if (this.splitCor(this.getCordianate(hckey)) > 1000) return "Trong 4 đến 5 ngày"
+    else if (this.splitCor(this.getCordianate(hckey)) > 500) return "Trong 3 đến 4 ngày"
+    else if (this.splitCor(this.getCordianate(hckey)) > 100) return "Trong 1 đến 2 ngày"
     else return "Trong vòng 1 ngày"
+    
   }
 }
