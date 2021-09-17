ba37f31cfff4ed73018990bfb6a8fe573c4f280f
shegga9x <74436981+shegga9x@users.noreply.github.com>
2021-09-17 09:18:47 +0700

    ok

--- AngularProject/src/app/Component/product-details/product-details.component.ts
+++ AngularProject/src/app/Component/product-details/product-details.component.ts
@@ -6,6 +6,7 @@ import { CartService } from 'src/app/service/cart.service';
 import { ThrowStmt } from '@angular/compiler';
 import { UserService } from 'src/app/service/user.service';
 import { User } from 'src/app/models/user';
+import { CartItem } from 'src/app/models/cart-item';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -30,38 +31,47 @@ export class ProductDetailsComponent implements OnInit {
   currentUser: User | null = null;
   product: Product | undefined;
   measure: string[] = [];
-  iTailorObject: string[] =[];
+  iTailorObject: string[] = [];
   vestObject: string[] = [];
   pantObject: string[] = []
   constructor(private route: ActivatedRoute, private productService: ProductService, private cartService: CartService, private userService: UserService) { }
   ngOnInit(): void {
     const routeParams = this.route.snapshot.queryParamMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
-    if (productIdFromRoute < 0){
+    if (productIdFromRoute < 0) {
       this.currentUser = this.userService.getCurrentUser();
       if (this.currentUser != null) {
         this.cartService.getDataByUserId(this.currentUser.id).subscribe(data => data.forEach(element => {
-          if (element.product.id == productIdFromRoute){
+          if (element.product.id == productIdFromRoute) {
             this.product = element.product;
-            this.measure = this.listTrans(element.product.name.split('$')[1].split('{'),"measure")
+            this.measure = this.listTrans(element.product.name.split('$')[1].split('{'), "measure")
             this.iTailorObject = this.listTrans(element.product.name.split('$')[2].split('{'), "iTailorObject")
             this.vestObject = this.listTrans(element.product.name.split('$')[3].split('{'), "vestObject")
             this.pantObject = this.listTrans(element.product.name.split('$')[4].split('{'), "pantObject")
           }
         }))
-      }else{
+      } else {
+        let CookieCart: any = localStorage.getItem("CookieCart");
+        let listCartItem: CartItem[] = JSON.parse(CookieCart) as CartItem[];
+        listCartItem.forEach(element => {
+          if (element.product.id == productIdFromRoute) {
+            this.product = element.product;
+            this.measure = this.listTrans(element.product.name.split('$')[1].split('{'), "measure")
+            this.iTailorObject = this.listTrans(element.product.name.split('$')[2].split('{'), "iTailorObject")
+            this.vestObject = this.listTrans(element.product.name.split('$')[3].split('{'), "vestObject")
+            this.pantObject = this.listTrans(element.product.name.split('$')[4].split('{'), "pantObject")
+          }
+        });
       }
-
-    }else{
+    } else {
       this.productService.getSingleProduct(productIdFromRoute).subscribe(product => {
         this.product = product;
       })
     }
-      // Find the product that correspond with the id provided in route.
-  
-  } 
+    // Find the product that correspond with the id provided in route.
+  }
   listTrans(list: string[], type: string): string[] {
-    let list1 :string[] = [];
+    let list1: string[] = [];
     for (let i = 0; i < list.length; i++) {
       let list2: string[] = list[i].replace(/[^a-zA-Z-0-9-.]/g, " ").split(' ');
       for (let i1 = 0; i1 < list2.length; i1++) {
@@ -71,20 +81,20 @@ export class ProductDetailsComponent implements OnInit {
     }
     let list1b: string[] = [];
     for (let i3 = 0; i3 < list1.length - 1; i3++) {
-      if((i3 %2)) {
-        if (list1[i3 + 1] == '$') list1b.push(list1[i3] )
+      if ((i3 % 2)) {
+        if (list1[i3 + 1] == '$') list1b.push(list1[i3])
         else list1b.push(list1[i3] + ": " + list1[i3 + 1])
       }
-   
+
     }
-    
+
     return list1b;
   }
   change(input: any) {
     let num = Number(input.value);
     if (num < 1) {
       input.value = 1;
-    }    
+    }
   }
   getProductAndQuantity(input: any, product: Product) {
     this.preventAbuse = true;
--- AngularProject/src/app/service/city.service.ts
+++ AngularProject/src/app/service/city.service.ts
@@ -36,121 +36,14 @@ export class CityService {
       })
     });
   }
-<<<<<<< HEAD
-  getCoordinate(hcKey: number): string {
-    let cordinate: string = "";
-    switch (hcKey) {
-      case 30:
-        cordinate = "20.939225337630926, 106.31275473807591";
-        break;
-      case 1:
-        cordinate = "21.03443273235992, 105.82254345647135";
-        break;
-      case 48:
-        cordinate = "16.057723866798206, 108.17838121453185";
-        break;
-      case 77:
-        cordinate = "10.28346637496381, 106.03150239673647";
-        break;
-      case 49:
-        cordinate = "15.677823173416828, 107.61436382449152";
-        break;
-      case 74:
-        cordinate = "11.284423314418255, 106.3652993094015";
-        break;
-      case 26:
-        cordinate = "21.457557333791172, 105.39551102583525";
-        break;
-      case 20:
-        cordinate = "21.854594515782832, 106.71713318030874";
-        break;
-      case 42:
-        cordinate = "18.37998194796159, 105.85942924074982";
-        break;
-      case 22:
-        cordinate = "21.34784813334017, 106.51270726269973";
-        break;
-      case 72:
-        cordinate = "20.425913954821052, 106.50965885608406";
-        break;
-      case 33:
-        cordinate = "20.677434589668074, 106.03413411525838";
-        break;
-      case 91:
-        cordinate = "9.994011372733805, 104.16893714363025";
-        break;
-      case 34:
-        cordinate = "20.46033624681186, 106.30705766832301";
-        break;
-      case 24:
-        cordinate = "21.302067651284702, 106.17949140796979";
-        break;
-      case 79:
-        cordinate = "10.826760025273845,106.6823606966149";
-        break;
-      case 36:
-        cordinate = "20.43448380422534, 106.08793192698147";
-        break;
-      case 89:
-        cordinate = "10.674590124286668, 105.01607942299779";
-        break;
-      case 94:
-        cordinate = "9.626460029135632, 105.94159052267136";
-        break;
-      case 86:
-        cordinate = "10.271819699230967, 105.90716365847219";
-        break;
-      case 45:
-        cordinate = "16.725182613065336, 107.09077493453262";
-        break;
-      case 31:
-        cordinate = "20.85395218342969, 106.69393949260603";
-        break;
-      case 84:
-        cordinate = "9.97173222073517, 106.34073321362116";
-        break;
-      case 19:
-        cordinate = "21.585663568200832, 105.7900973667736";
-        break;
-      case 96:
-        cordinate = "9.200776642645451, 105.1682859785899";
-        break;
-      case 25:
-        cordinate = "21.45126547048769, 105.24112478720403";
-        break;
-      case 51:
-        cordinate = "15.168344519876664, 108.81796793149348";
-        break;
-      case 46:
-        cordinate = "16.41211210909523, 107.35117449778903";
-        break;
-      case 15:
-        cordinate = "16.070370406916023, 108.2213519715551";
-        break;
-      case 10:
-        cordinate = "22.465081276319474, 103.98806683937451";
-        break;
-      case 2:
-        cordinate = "22.79424724564605, 104.98187279046168";
-        break;
-      case 8:
-        cordinate = "21.82260168132014, 105.16175223082817";
-        break;
-      case 12:
-        cordinate = "22.399630554854806, 103.43726575924691";
-        break;
-      case 14:
-        cordinate = "21.367293656643824, 103.88760460682506";
-        break;
-      case 52:
-        cordinate = "14.392838068725473, 108.61966876769002";
-=======
+
+
   getCordianate(hcKey: string): string {
     let cordinate: string = "";
     switch (hcKey) {
       case "79":
         cordinate = "10.823512334224576,106.61446278562542";
->>>>>>> dc57662138d03797e02409e33c1c5c9893319bbd
+
         break;
       case "36":
         cordinate = "20.43834787202179,106.16174700901708";
